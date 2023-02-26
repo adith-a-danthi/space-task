8e05583c750b18b5bd104c3718681bcba4d31207
Adith A Danthi <adith.danthi@gmail.com>
2023-02-27 00:30:54 +0530

    ♻️ refactor: update hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,14 +4,14 @@ export default function Hero() {
   return (
     <section className="hero">
       <div className="content hero__content">
-        <div className='extra_content'>
+        <div className="extra_content">
           <img
             className="hero__image portrait_image"
             src="https://images.unsplash.com/photo-1593314731059-d6eb89748e65?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
             alt="Woman standing in the middle of a forest"
           />
         </div>
-        <div>
+        <div className="main__content">
           <h1 className="hero__title heading-1">
             Requirements gathering and analysis for references.
           </h1>
@@ -23,7 +23,7 @@ export default function Hero() {
           </p>
 
           <img
-            className="hero__image landscape"
+            className="hero__image landscape_image"
             src="https://images.unsplash.com/photo-1586798274322-2e6b63a43a0e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1149&q=80"
             alt="Woman standing in the middle of a forest"
           />
