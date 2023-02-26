9d05f21d9f759da84f7407ad76b7f8bcb0b25246
Adith A Danthi <adith.danthi@gmail.com>
2023-02-26 16:46:42 +0530

    💄 feat: add testimonial item

---
+++ src/components/TestimonialItem/TestimonialItem.jsx
@@ -0,0 +1,21 @@
+import './TestimonialItem.css';
+
+export default function TestimonialItem({ testimonial }) {
+  return (
+    <div className="testimonial" style={{ backgroundColor: testimonial.color }}>
+      <h2 className="testimonial__title">{testimonial.title}</h2>
+
+      <div className="testimonial__author">
+        <img
+          className="testimonial__avatar"
+          src={testimonial.avatar}
+          alt="avatar"
+        />
+        <div>
+          <h3 className="testimonial__name">{testimonial.name}</h3>
+          <p className="testimonial__role">{testimonial.role}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
