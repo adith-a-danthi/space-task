7d8f0f78b9d5586ded9f0cc366e179720980c650
Adith A Danthi <adith.danthi@gmail.com>
2023-02-26 12:25:53 +0530

    ✨ feat: add navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import './Navbar.css';
+
+const navLinks = [
+  { title: 'Home' },
+  { title: 'Templates' },
+  { title: 'Price' },
+  { title: 'Help' },
+];
+
+export default function Navbar() {
+  return (
+    <nav class="nav">
+      <div className="nav__content">
+        <div className="nav__logo">Space</div>
+        <div className="nav__links">
+          {navLinks.map((link, index) => {
+            return (
+              <div className="nav__link" key={index} href="#">
+                {link.title}
+              </div>
+            );
+          })}
+          <button className="nav__button">Get In Touch</button>
+        </div>
+      </div>
+    </nav>
+  );
+}
